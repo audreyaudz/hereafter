7ea708763e05cc75aadece22123a4511edbb53f5
audreyaudz <audz@kon.sg>
2022-05-27 16:04:27 +0800

    terms of service page made

--- src/components/pages/TermsOfService.vue
+++ src/components/pages/TermsOfService.vue
@@ -5,13 +5,76 @@
 </script>
 
 <template>
-  <i-layout class="centerdiv h-full text-center">
+  <i-layout class="h-full _text-align:left ml-3 mr-3">
   <i-layout-header>
   <h1> Terms of Service </h1>
   </i-layout-header>
-  <i-layout-content class="text-left ml-6 mb-16 ">
-    Content
-  </i-layout-content>
+  <p class="mb-6 _font-weight:bold _font-size:lg">
+    Here:After
+  </p>
+  <p class="mb-6 _font-size:sm">
+    Please read these terms of service ("terms", "terms of service") carefully before using <b>Here:After</b> website (the "service") operated by <b>Here:After</b> ("us", 'we", "our").
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Conditions of Use
+  </p>
+  <p class="mb-6 _font-size:sm">
+    We will provide their services to you, which are subject to the conditions stated below in this document. Every time you visit this website, use its services or make a purchase, you accept the following conditions. This is why we urge you to read them carefully.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Privacy Policy
+  </p>
+  <p class="mb-6 _font-size:sm">
+    Before you continue using our website we advise you to read our privacy policy [link to privacy policy] regarding our user data collection. It will help you better understand our practices.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Copyright
+  </p>
+  <p class="mb-6 _font-size:sm">
+    Content published on this website (digital downloads, images, texts, graphics, logos) is the property of <b>Here:After</b> and/or its content creators and protected by international copyright laws. The entire compilation of the content found on this website is the exclusive property of <b>Here:After</b>, with copyright authorship for this compilation by <b>Here:After</b>.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Communications
+  </p>
+  <p class="mb-6 _font-size:sm">
+    The entire communication with us is electronic. Every time you send us an email or visit our website, you are going to be communicating with us. You hereby consent to receive communications from us. If you subscribe to the news on our website, you are going to receive regular emails from us. We will continue to communicate with you by posting news and notices on our website and by sending you emails. You also agree that all notices, disclosures, agreements, and other communications we provide to you electronically meet the legal requirements that such communications be in writing.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Applicable Law
+  </p>
+  <p class="mb-6 _font-size:sm">
+    By visiting this website, you agree that the laws of the Singapore, without regard to principles of conflict laws, will govern these terms of service, or any dispute of any sort that might come between <b>Here:After</b> and you, or its business partners and associates.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Disputes
+  </p>
+  <p class="mb-6 _font-size:sm">
+    Any dispute related in any way to your visit to this website shall be arbitrated by state or federal court Singapore and you consent to exclusive jurisdiction and venue of such courts.
+  </p>
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    Comments, Reviews, and Emails
+  </p>
+  <p class="mb-6 _font-size:sm">
+    Visitors may post content as long as it is not obscene, illegal, defamatory, threatening, infringing of intellectual property rights, invasive of privacy, or injurious in any other way to third parties. Content has to be free of software viruses, political campaigns, and commercial solicitation.
+    <br>
+    We reserve all rights (but not the obligation) to remove and/or edit such content. When you post your content, you grant <b>Here:After</b> non-exclusive, royalty-free and irrevocable right to use, reproduce, publish, modify such content throughout the world in any media.
+  </p>
+
+  <p class="mb-2 _font-weight:bold _font-size:lg">
+    License and Site Access
+  </p>
+  <p class="mb-16 _font-size:sm">
+    We grant you a limited license to access and make personal use of this website. You are not allowed to download or modify it. This may be done only with written consent from us.
+    <br>
+    We reserve all rights to terminate accounts, edit or remove content at their sole discretion.
+  </p>
+  </i-layout>
+
+  <i-layout class="centerdiv h-full text-left p-10 bg-amber-50">
+    <p class="_text-align:center _font-weight:bold _font-size:xl ">Get in touch with us</p>
+    <p class="_text-align:center _font-size:md pb-6">Contact us if you have a story to share, feedback for us, or are interested in joining us!</p>
+      <i-button class="centerdiv w-20 mt-20 px-20" outline color="dark" href="https://inkline.io">Contact Us
+      </i-button>
   </i-layout>
 
 </template>
