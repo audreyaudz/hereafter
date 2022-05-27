dc81d51cbcdd3d5b89b4519eba17fb1db486c129
audreyaudz <audz@kon.sg>
2022-05-27 23:01:04 +0800

    404 page done

--- src/components/pages/FourOhFour.vue
+++ src/components/pages/FourOhFour.vue
@@ -1,14 +1,19 @@
 <!-- This is the About page ('/about') of Here:After -->
 
 <template>
-
-  <i-layout class="centerdiv h-full text-center">
+  <i-layout class="centerdiv">
   <i-layout-header>
-  <h1> Page Does Not Exist </h1>
+  <h1 class="text-4xl text-center"> 404 </h1>
   </i-layout-header>
-  <i-layout-content class="text-left ml-6 mb-16 ">
-    Oh No
+  <i-layout-content class="text-center text-2xl">
+    Looks like you are lost!
   </i-layout-content>
+  <i-layout-content class="centerdiv">
+    <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" >
+  </i-layout-content>
+    <i-button href="/" color="success" class="centerdiv w-25 _margin-bottom:3">
+        Let's Go Home
+    </i-button>
   </i-layout>
 
 </template>
