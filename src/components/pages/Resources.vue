c9ebddb69f0713d595e282ff5a96fd32b48d91b3
audreyaudz <audz@kon.sg>
2022-05-30 01:36:04 +0800

    resources amended

--- src/components/pages/Resources.vue
+++ src/components/pages/Resources.vue
@@ -20,31 +20,30 @@
       <i-collapsible color="bg-browned ">
       <i-collapsible-item title="Advanced Care Plan (ACP)">
         <i-nav vertical>
-          <i-nav-item href="https://caretoplay.sg/" target="_blank">Care To Play Website</i-nav-item>
-          <i-nav-item href="https://mylegacy.life.gov.sg/" target="_blank">My Legacy Website</i-nav-item>
-          <i-nav-item href="https://livingwishessg.com/acp/" target="_blank">Living Wishes Website</i-nav-item>
-          <i-nav-item href="https://shenghong.org.sg/life-point/centre-profile/" target="_blank" >Sheng Hong Website</i-nav-item>
-          <i-nav-item href="https://www.aic.sg/care-services/other-ways-to-plan-ahead" target="_blank">AIC Website - Care Services</i-nav-item>
+          <i-nav-item href="https://www.aic.sg/care-services/other-ways-to-plan-ahead" target="_blank">Agency for Integrated Care Website - Care Services</i-nav-item>
+          <i-nav-item href="https://caretoplay.sg/" target="_blank">Care To Play Website - ACP Quiz </i-nav-item>
+          <i-nav-item href="https://livingwishessg.com/acp/" target="_blank">Living Wishes Website - Advanced Care Planning</i-nav-item>
+          <i-nav-item href="https://mylegacy.life.gov.sg/" target="_blank">My Legacy Website - For End of Life Planning</i-nav-item>
+          <i-nav-item href="https://shenghong.org.sg/life-point/centre-profile/" target="_blank" >Sheng Hong Website - Helps assist elderly above 50 with weak or no social and family support</i-nav-item>
         </i-nav>
       </i-collapsible-item>
       <i-collapsible-item title="Advanced Medical Directive (AMD)">
         <i-nav vertical>
-          <i-nav-item href="https://www.moh.gov.sg/policies-and-legislation/advance-medical-directive" target="_blank">MOH Website - AMD</i-nav-item>
+          <i-nav-item href="https://www.moh.gov.sg/policies-and-legislation/advance-medical-directive" target="_blank">Ministry of Health Website - AMD</i-nav-item>
           <i-nav-item href="https://sso.agc.gov.sg/Act/AMDA1996" target="_blank">Singapore Statutes Online Website - AMD Act 1996</i-nav-item>
         </i-nav>
       </i-collapsible-item>
 
       <i-collapsible-item title="Funeral Guides">
         <i-nav vertical>
-          <i-nav-item href="https://www.aic.sg/caregiving/prepare-funeral" target="_blank">AIC Website - How to Prepare for a Funeral</i-nav-item>
-          <i-nav-item href="https://www.nea.gov.sg/our-services/after-death/post-death-matters" target="_blank">NEA Website - Post Death Matters</i-nav-item>
-          <i-nav-item href="https://www.aic.sg/caregiving/prepare-funeral" target="_blank">AIC Website - How to Prepare for a Funeral</i-nav-item>
+          <i-nav-item href="https://www.aic.sg/caregiving/prepare-funeral" target="_blank">Agency for Integrated Care Website - How to Prepare for a Funeral</i-nav-item>
+          <i-nav-item href="https://www.nea.gov.sg/our-services/after-death/post-death-matters" target="_blank">National Environment Agency Website - Post Death Matters</i-nav-item>
         </i-nav>
       </i-collapsible-item>
 
       <i-collapsible-item title="Inheritance / Legacy Planning">
         <i-nav vertical>
-          <i-nav-item href="https://pto.mlaw.gov.sg/deceased-cpf-estate-monies/information-for-next-of-kin-estate-monies/" target="_blank">Ministry of Law - Estate(Other Assets)</i-nav-item>
+          <i-nav-item href="https://pto.mlaw.gov.sg/deceased-cpf-estate-monies/information-for-next-of-kin-estate-monies/" target="_blank">Ministry of Law Website - Estate(Other Assets)</i-nav-item>
           <i-nav-item href="https://mylegacy.life.gov.sg/end-of-life-planning/write-a-will/how-to-write-a-will/" target="_blank">My Legacy Website - How To Write A Will</i-nav-item>
         </i-nav>
       </i-collapsible-item>
@@ -53,18 +52,17 @@
         <i-nav vertical>
           <i-nav-item href="https://www.healthhub.sg/live-healthy/84/coping_with_bereavement" target="_blank">HealthHub Website - Coping with Bereavement for Better Mental Wellness</i-nav-item>
           <i-nav-item href="https://www.imh.com.sg/Mental-Health-Resources/Conditions-and-Challenges/Pages/Coping-with-Bereavement.aspx" target="_blank">Institute of Mental Health Website - Coping with Bereavement</i-nav-item>
+          <i-nav-item href="https://www.nccs.com.sg/patient-care/specialties-services/support-groups" target="_blank">National Cancer Centre Website - Patient Support Groups</i-nav-item>
           <i-nav-item href="https://sacsingapore.org/registry/find-a-counsellor/wpbdp_category/grieflossdepression/" target="_blank">Singapore Association for Counselling - Find a counsellor</i-nav-item>
           <i-nav-item href="https://www.imh.com.sg/Mental-Health-Resources/Conditions-and-Challenges/Pages/Coping-with-Bereavement.aspx" target="_blank">Singapore Hospice Council Website - Community Bereavement Service Providers</i-nav-item>
-          <i-nav-item href="https://www.imh.com.sg/Mental-Health-Resources/Conditions-and-Challenges/Pages/Coping-with-Bereavement.aspx" target="_blank">National Cancer Centre Website - Patient Support Groups</i-nav-item>
         </i-nav>
 </i-collapsible-item>
 
       <i-collapsible-item title="Palliative Care">
         <i-nav vertical>
-          <i-nav-item href="https://library.singaporehospice.org.sg/?docs=faqs-palliative-care/" target="_blank">Singapore Hospice Council Website - FAQs on Palliative Care</i-nav-item>
-          <i-nav-item href="https://www.assisihospice.org.sg/patients/" target="_blank">Assisi Hospice Website - Palliative Care Services</i-nav-item>
           <i-nav-item href="https://www.aic.sg/care-services/inpatient-hospice-palliative-care-service/" target="_blank">Agency for Integrated Care Website - Inpatient Hospice Palliative Care Service</i-nav-item>
-        </i-nav>
+          <i-nav-item href="https://www.assisihospice.org.sg/patients/" target="_blank">Assisi Hospice Website - Palliative Care Services</i-nav-item>
+          <i-nav-item href="https://library.singaporehospice.org.sg/?docs=faqs-palliative-care/" target="_blank">Singapore Hospice Council Website - FAQs on Palliative Care</i-nav-item>        </i-nav>
       </i-collapsible-item>
     </i-collapsible>
     </i-layout>
